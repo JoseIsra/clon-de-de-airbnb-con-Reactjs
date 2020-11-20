afd4af22344db875b9f0e0076f3d7b07e2f4ceef
israel diaz <israelmyname@gmail.com>
2020-11-20 15:50:46 -0500

    se avanzó segunda parte del banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Banner.css';
+import { Button } from '@material-ui/core';
+import Search from '../components/Search';
 function Banner() {
+    const [showSearch , setShowSearch] =useState(false);
+
     return (
         <div className="banner">
+            <div className="banner__search">
+
+            <Button onClick = {()=>setShowSearch(!showSearch)}
+            className="banner__searchButton"
+            variant ="outlined"
+            >
+                {showSearch ? "Ocultar": "Buscar fechas"}
+            </Button>
+            </div>
+            {showSearch && <Search />}
+
+
+
             <div className="banner__info">
                 <p>Siéntete un <br />
                     turista cerca <br/> 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import '../styles/Home.css';
 import Banner from './Banner';
+import Card from './Card';
 
 
 function Home() {
     return (
         <div className="home">
             <Banner />
+            <div className="home__section">
+                <Card  src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=240"
+                title="Alojamientos enteros"
+                description ="más de 300 alojamientos"
+                />
+                {/* <Card /> */}
+                {/* <Card /> */}
+            </div>
+          {/*   <div className="home__section">
+                <Card />
+                <Card />
+                <Card />
+            </div> */}
+
         </div>
     )
 }
